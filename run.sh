25c5c8f6f22a35db8de63cda0ba0028fe25664b7
Dheeraj KS <dheerajks@ZTLM02.local>
2022-12-01 16:43:52 +0530

    added .sh file

--- run.sh
+++ run.sh
@@ -7,7 +7,6 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | b
 nvm install 4.4.5
 
 node -v 
-# node --version
-# npm install
-# npx wdio run wdio.conf.js
+npm install
+npx wdio run wdio.conf.js
 exit 0
