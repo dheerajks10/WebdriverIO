74aeb70012bcc99f29179f9371b5cb0641b516eb
Dheeraj KS <dheerajks@ZTLM02.local>
2022-12-01 16:42:44 +0530

    added .sh file

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 #! /bin/sh
 echo "jenkines started...!"
-node --version
-npm install
-npx wdio run wdio.conf.js
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
+
+. ~/.nvm/nvm.sh
+
+nvm install 4.4.5
+
+node -v 
+# node --version
+# npm install
+# npx wdio run wdio.conf.js
 exit 0
